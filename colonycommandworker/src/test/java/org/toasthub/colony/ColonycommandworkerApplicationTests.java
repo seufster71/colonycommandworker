e8a1e3ba0d4d49f4ce6202d3ffe525c6d314e56b
seufster71 <seufster@gmail.com>
2023-05-18 09:25:47 -0400

    test

---
+++ colonycommandworker/src/test/java/org/toasthub/colony/ColonycommandworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toasthub.colony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColonycommandworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
