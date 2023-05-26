6a05b3c5172a518821c0f45eed2c49d0d2530402
seufster71 <seufster@gmail.com>
2023-05-26 09:14:28 -0400

    init buildout for colony

--- colonycommandworker/gitcommitall.sh
+++ colonycommandworker/gitcommitall.sh
@@ -20,58 +20,58 @@ checkAndCommit() {
 
 commitAll() {
 	ECHO "#### admin"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/admin
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/admin
 	checkAndCommit "${1}"
 
 	ECHO "#### adminView"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/adminView
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/adminView
 	checkAndCommit "${1}"
 	
 	ECHO "#### core"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/core
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/core
 	checkAndCommit "${1}"
 	
 	ECHO "#### coreView"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/coreView
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/coreView
 	checkAndCommit "${1}"
 	
 	ECHO "#### system"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/system
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/system
 	checkAndCommit "${1}"
 	
 	ECHO "#### systemView"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/systemView
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/systemView
 	checkAndCommit "${1}"
 	
 	ECHO "#### member logout"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/member/logout
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/member/logout
 	checkAndCommit "${1}"
 	ECHO "#### member logout view"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/memberView/logout
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/memberView/logout
 	checkAndCommit "${1}"
 	
 	ECHO "#### member profile"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/member/profile
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/member/profile
 	checkAndCommit "${1}"
 	ECHO "#### member profile view"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/memberView/profile
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/memberView/profile
 	checkAndCommit "${1}"
 	
 	ECHO "#### member session"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/member/session
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/member/session
 	checkAndCommit "${1}"
 	
 	ECHO "#### member colony"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/member/colony
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/member/colony
 	checkAndCommit "${1}"
 	
 	ECHO "#### member colony view"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker/src/main/js/memberView/colony
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker/src/main/js/memberView/colony
 	checkAndCommit "${1}"
 	
 	
 	ECHO "#### main"
-	cd ~/$GIT_DIR/colonycommandworker/colonycommandworker
+	cd ~/$GIT_DIR/colonycommanderworker/colonycommandworker
 	checkAndCommit "${1}"
 	
 	ECHO "#### Done committing"
